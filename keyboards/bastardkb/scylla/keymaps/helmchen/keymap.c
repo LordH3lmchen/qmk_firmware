c29a25a36f5163fa02209e0252ac34ed677e9fb4
Florian Trabauer <florian@trabauer.com>
2025-11-09 16:15:22 +0100

    Added Super Key to the Tab Key
    I've usually mapped that with keyd. But I use qmk Keyboards for Gaming
    and work now. So each is configured to my needs.

--- keyboards/bastardkb/scylla/keymaps/helmchen/keymap.c
+++ keyboards/bastardkb/scylla/keymaps/helmchen/keymap.c
@@ -21,7 +21,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [0] = LAYOUT_split_4x6_5(KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS,
                              //-------------------------------------------------//-----------------------------------------------------------//
-                             KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
+                             MT(KC_LGUI,KC_TAB), KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS,
                              //-------------------------------------------------//-----------------------------------------------------------//
                              MT(MOD_LCTL,KC_ESC), MT(MOD_LSFT,KC_A), MT(MOD_LALT,KC_S), KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, MT(MOD_RSFT,KC_SCLN), MT(MOD_RCTL,KC_QUOT),
                              //-------------------------------------------------//-----------------------------------------------------------//
